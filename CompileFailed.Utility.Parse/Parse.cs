5106d5d7fcb8c9ee58bb9412c800890dc863d2ff
Michael Harry <michael@compilefailed.com>
2017-12-23 21:58:10 -0600

    Initial commit

---
+++ CompileFailed.Utility.Parse/Parse.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompileFailed.Utility
+{
+    public static class Parse
+    {
+        /// <summary>
+        /// Parses a string to the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type to parse the string to.</typeparam>
+        /// <param name="value">The string to parse.</param>
+        /// <returns>T, which is the parsed value of the string.</returns>
+        public static T ParseString<T>(String value)
+        {
+            return (T)Convert.ChangeType(value, typeof(T));
+        }
+
+        /// <summary>
+        /// Parses a string to the specified nullable type,
+        /// returning null if a FormatException occurs during parsing.
+        /// </summary>
+        /// <typeparam name="T">The type to parse the string to.</typeparam>
+        /// <param name="value">The string to parse.</param>
+        /// <returns>Nullable<T>, which is the parsed value of the string,
+        /// or null if a FormatException or InvalidCastException occurs
+        /// during parsing.</returns>
+        public static T? ParseStringNullable<T>(String value) where T : struct
+        {
+            try
+            {
+                return (T)Convert.ChangeType(value, typeof(T));
+            }
+            catch (FormatException)
+            {
+                return null;
+            }
+            catch (InvalidCastException)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ CompileFailed.Utility.ParseTests/ParseTest.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompileFailed.Utility.ParseTests
+{
+    [TestFixture]
+    public class ParseTest
+    {
+        [Test]
+        public void ParseStringShouldReturnParsedValue()
+        {
+            var expected = new DateTime(2017, 12, 25);
+            var actual = Parse.ParseString<DateTime>("12-25-17");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ParseStringShouldThrowFormatExceptionIfValueInvalid()
+        {
+            Assert.Throws<FormatException>(() => Parse.ParseString<Decimal>("This is not a decimal."));
+        }
+
+        [Test]
+        public void ParseStringNullableShouldNotBeNullIfValueValid()
+        {
+            var actual = Parse.ParseStringNullable<Decimal>("3.14159");
+            Assert.NotNull(actual);
+        }
+
+        [Test]
+        public void ParseStringNullableShouldReturnNullIfValueInvalid()
+        {
+            var actual = Parse.ParseStringNullable<Guid>("This is not a guid.");
+            Assert.IsNull(actual);
+        }
+    }
+}
